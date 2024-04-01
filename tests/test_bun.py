f7948ab3864521c0b9f60a617346383d78ab30f0
Anton Chernichkin <tonynba@yandex.ru>
2024-04-01 22:31:25 +0200

    Добавил структуру и тесты для Bun

---
+++ tests/test_bun.py
@@ -0,0 +1,13 @@
+import pytest
+from bun import Bun
+
+
+class TestBunCollector:
+
+    def test_get_name_add_new_name(self):
+        bun = Bun("Prrpr", 2)
+        assert bun.get_name() == "Prrpr"
+
+    def test_get_price_add_new_price(self):
+        bun = Bun("Prrpr", 2)
+        assert bun.get_price() == 2
